9cb299d71f09b5b30bec319afff0727b1d6c91c3
Waldo-Blom <waldoblom2004@gmail.com>
2025-10-07 18:44:16 +0200

    Added intial forms and folder structure

---
+++ Sky_High_Super_Hero_Academy/Sky_High_Super_Hero_Academy/PresentationLayer/HeroSummary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sky_High_Super_Hero_Academy.PresentationLayer
+{
+    public partial class HeroSummary : Form
+    {
+        public HeroSummary()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sky_High_Super_Hero_Academy/Sky_High_Super_Hero_Academy/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sky_High_Super_Hero_Academy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Sky_High_Super_Hero_Academy")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("193bbbcd-849f-492d-945e-15b733f6f515")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
