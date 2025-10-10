cfcde9c26ef4dd2cc7fccb88502fd15d5adb0e1c
Waldo-Blom <waldoblom2004@gmail.com>
2025-10-10 18:17:30 +0200

    Create initial Superhero FileHandler and FormatHandler class files.

---
+++ Sky_High_Super_Hero_Academy/Sky_High_Super_Hero_Academy/DataAccessLayer/Superhero_FileHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sky_High_Super_Hero_Academy.DataAccessLayer
+{
+    internal class Class1
+    {
+    }
+}
